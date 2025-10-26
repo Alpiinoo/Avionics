9fb52453b9ef6c7e005acc27d5148204083997a5
Alp Tekin <alptekin656@gmail.com>
2025-10-26 12:35:27 +0200

    Add Core/SimCore.h and fix case sensitivity

---
+++ Avionics/Core/SimCore.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <QObject>
+#include <QTimer>
+#include "../Engine/Engine.h"
+#include "../FADEC/FADEC.h"
+
+class SimCore : public QObject
+{
+	Q_OBJECT
+
+public:
+	explicit SimCore(QObject* parent = nullptr);
+
+	Engine engine1;
+	Engine engine2;
+
+	FADEC fadec;
+
+	double pitch_deg;
+	double roll_deg;
+	double heading_deg;
+	double altitude_ft;
+	double speed_kts;
+
+public slots:
+	void update();
+
+signals:
+	void updated();
+
+private:
+	QTimer timer;
+};
